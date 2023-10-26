2e0d53db005e21024454b826bc857f08da3cc75c
Marius ROBERT <mariusrobert380@gmail.com>
2023-10-26 23:07:31 +0200

    created symlink for constants

--- front/docker-storage/src/constants.ts
+++ front/docker-storage/src/constants.ts
@@ -1,4 +1,2 @@
-export const frontURL = 'http://localhost:3000';
-export const apiURL = 'http://localhost:3001'
-// export const frontURL = 'https://webstats.pening.fr';
-// export const apiURL = 'https://paco.pening.fr:6930'
+export const frontURL = 'https://paco.pening.fr:6980';
+export const apiURL = 'https://paco.pening.fr:6981'
