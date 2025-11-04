f61769f6e0a64b44b5b193fd1dd707e19560c669
Marius ROBERT <mariusrobert380@gmail.com>
2025-11-04 17:33:03 +0100

    Initialized the Sveltekit project

---
+++ svelte/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import '../app.css';
+  import * as Card from "$lib/components/ui/card/index.js";
+  import favicon from '$lib/assets/favicon.svg';
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+    <link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="w-scren h-screen flex items-center justify-center bg-red-500">
+  <Card.Root class="shadow-2xl">
+    <Card.Content>
+      {@render children()}
+    </Card.Content>
+  </Card.Root>
+</div>
---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="mb-2 p-2">Welcome to <br/> Secret Santa</h1>
