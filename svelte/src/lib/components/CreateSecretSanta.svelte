34bb7f7e5c214bc49f9ba190ae09b957effff782
Marius ROBERT <mariusrobert380@gmail.com>
2025-11-06 18:12:25 +0100

    Finished the create santa dialog

--- svelte/src/lib/components/CreateSecretSanta.svelte
+++ svelte/src/lib/components/CreateSecretSanta.svelte
@@ -5,12 +5,23 @@
   import { Calendar } from '$lib/components/ui/calendar';
   import { Button } from '$lib/components/ui/button';
   import { Label } from '$lib/components/ui/label';
+  import { goto } from '$app/navigation';
 
   let name: string = $state('');
   let mailDate: CalendarDate = $state(today(getLocalTimeZone()).add({ weeks: 1 }));
   let formattedMailDate = $derived(mailDate.toDate(getLocalTimeZone()));
 
   let isCalendarPopoverOpen = $state(false);
+
+  function createSecretSanta() {
+    fetch('/api/v1/santa', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        date: formattedMailDate,
+      }),
+    }).then((r) => r.json()).then(({ code }) => goto(`/${code}`));
+  }
 </script>
 
 <div class="flex justify-evenly flex-col gap-3 mt-4">
@@ -22,16 +33,16 @@
 
   <div class="flex flex-col gap-2">
     <Label for="mailDate">Mail sending date</Label>
+    <Button
+      variant="outline"
+      class="w-full text-black justify-between font-normal"
+      id="mailDate"
+      onclick={() => isCalendarPopoverOpen = !isCalendarPopoverOpen}
+    >
+      {mailDate.toDate(getLocalTimeZone()).toLocaleDateString()}
+    </Button>
     <Popover.Root bind:open={isCalendarPopoverOpen}>
-      <Popover.Trigger>
-        <Button
-          variant="outline"
-          class="w-full text-black justify-between font-normal"
-          id="mailDate"
-        >
-          {mailDate.toDate(getLocalTimeZone()).toLocaleDateString()}
-        </Button>
-      </Popover.Trigger>
+      <Popover.Trigger>Test</Popover.Trigger>
       <Popover.Content class="w-auto mx-auto overflow-hidden p-0" align="start">
         <Calendar
           type="single"
@@ -49,7 +60,7 @@
   <div class="w-full justify-end flex">
     <Button
       disabled={!name.length}
-      onClick={() => {/* TODO: add the fetch and handle response */}}
+      onclick={createSecretSanta}
     >
       Create
     </Button>
