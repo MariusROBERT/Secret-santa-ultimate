676baee0243751ee4c331e66c2f4a4590304dd48
marobert <marobert@student.42lyon.fr>
2025-12-11 15:59:59 +0100

    Redirect to home page if invalid code

--- svelte/src/routes/[code]/+page.js
+++ svelte/src/routes/[code]/+page.js
@@ -1,5 +1,11 @@
+import {redirect} from "@sveltejs/kit";
+
 export async function load({ params, fetch }) {
   return await fetch(`/api/v1/santa/${params.code}`).then(
-    (/** @type {{ json: () => any; }} */ res) => res.json(),
+    (/** @type {{ json: () => any; }} */ res) => {
+      if (res.ok)
+        return res.json()
+      redirect( 307,'/')
+    },
   );
 }
