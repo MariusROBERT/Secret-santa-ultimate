0f9abe73b73e4c37eed7804881664fe67b334369
MariusROBERT <mariusrobert380@gmail.com>
2025-12-12 15:14:55 +0100

    Added zod verification for new users in backend

--- svelte/src/routes/api/v1/santa/[code]/users/+server.js
+++ svelte/src/routes/api/v1/santa/[code]/users/+server.js
@@ -1,5 +1,6 @@
 import { error, json } from '@sveltejs/kit';
 import { addUsers } from '$lib/db/utils/secretSanta.js';
+import { z } from 'zod';
 
 /**@typedef {import('@sveltejs/kit').RequestEvent} RequestEvent*/
 
@@ -11,6 +12,17 @@ export async function POST({ request, params }) {
   const { name, mail } = await request.json();
   const { code } = params;
   if (!name || !mail || !code) throw error(422, { message: 'missing fields' });
+
+  const newUserSchema = z.object({
+    name: z.string().min(1),
+    mail: z.email(),
+  });
+
+  try {
+    newUserSchema.parse({ name: name, mail: mail });
+  } catch (e) {
+    throw error(422, { message: JSON.parse(e.message)[0].message });
+  }
 
   const user = await addUsers(code, mail, name);
 
